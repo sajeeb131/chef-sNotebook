ddd78523a924f3c911533d78a75ceee29ad3e503
Yeasin Hasan <amyasxtrem@gmail.com>
2024-12-11 18:50:32 +0600

    Homepage - updated (not complete)

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -3,6 +3,7 @@ import { CiSearch } from "react-icons/ci";
 import Image from 'next/image';
 import landingImage from '@/public/assets/l1.png'
 import './page.css'
+import '../components/Footer.jsx'
 import curveLine from '@/public/assets/curve-line.png'
 import pasta from '@/public/assets/homepage/pasta.jpeg'
 import pizza from '@/public/assets/homepage/chicken pizza.png'
@@ -13,6 +14,10 @@ import healthy from '@/public/assets/homepage/healthy.png'
 import desserts from '@/public/assets/homepage/desserts.png'
 import bbq from '@/public/assets/homepage/BBQ.png'
 import vegeterian from '@/public/assets/homepage/vegeterian.png'
+import collection1 from '@/public/assets/homepage/collection.png'
+import collection2 from '@/public/assets/homepage/collection-1.png'
+import collection3 from '@/public/assets/homepage/collection-2.png'
+import Footer from '../components/Footer.jsx';
 
 const page = () => {
   return (
@@ -48,15 +53,15 @@ const page = () => {
             </div>
             <div className="item">
               <Image  src={pizza} alt=''></Image>
-              <h2>Chicken Pasta</h2>
+              <h2>Chicken Pizza</h2>
             </div>
             <div className="item">
               <Image  src={soup} alt=''></Image>
-              <h2>Chicken Pasta</h2>
+              <h2>Tomato Basil Soup</h2>
             </div>
             <div className="item">
               <Image  src={butterMasala} alt=''></Image>
-              <h2>Chicken Pasta</h2>
+              <h2>Paneer Butter Masala</h2>
             </div>
             
             
@@ -67,27 +72,37 @@ const page = () => {
           <div className='items'>
             <div className="item">
                 <Image className='more-container-img' src={quicknEasy} alt=''></Image>
-                <h2>Chicken Pasta</h2>
+                <h2 className='more-container-title'>Quick & Easy</h2>
             </div>
             <div className="item">
                 <Image className='more-container-img' src={healthy} alt=''></Image>
-                <h2>Chicken Pasta</h2>
+                <h2 className='more-container-title'>Healthy</h2>
             </div>
             <div className="item">
                 <Image  src={bbq} alt=''></Image>
-                <h2>Chicken Pasta</h2>
+                <h2 className='more-container-title'>BBQ</h2>
             </div>
             <div className="item">
                 <Image  src={desserts} alt=''></Image>
-                <h2>Chicken Pasta</h2>
+                <h2 className='more-container-title'>Desserts</h2>
             </div>
             <div className="item">
                 <Image  src={vegeterian} alt=''></Image>
-                <h2>Chicken Pasta</h2>
+                <h2 className='more-container-title'>Vegeterian</h2>
+            </div>
+          </div>
+          
+          <div className="container-collection">
+            <h1 className='heading-1'>COLLECTIONS</h1>
+            <div className="container-collection-images">
+              <img src={collection1} alt="" />
+              <img src={collection2} alt="" />
+              <img src={collection3} alt="" />
             </div>
           </div>
         </div>
       </landing>
+      <Footer></Footer>
     </div>
   )
 }
